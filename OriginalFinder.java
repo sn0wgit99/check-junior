6a8f6d3d8cadde8fc30ea665c934909d803dffeb
Adilet <ninja2282011@gmail.com>
2025-03-25 19:37:52 +0600

    OriginalFinder

--- OriginalFinder.java
+++ OriginalFinder.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 public class OriginalFinder {
-    public static List<Integer> findOriginal(List<Integer> list) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
+    public static List<Integer> findOriginal(List<Integer> integer) {
+        Map<Integer, Integer> frequency = new HashMap<>();
         List<Integer> result = new ArrayList<>();
 
 
-        for (int number : list) {
-            frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) + 1);
+        for (int number : integer) {
+            frequency.put(number, frequency.getOrDefault(number, 0) + 1);
         }
 
 
-        for (int numebr : list) {
-            if (frequencyMap.get(numebr) > 1 && !result.contains(numebr)) {
+        for (int numebr : integer) {
+            if (frequency.get(numebr) > 1 && !result.contains(numebr)) {
                 result.add(numebr);
             }
         }
@@ -21,7 +21,7 @@ public class OriginalFinder {
     }
 
     public static void main(String[] args) {
-        List<Integer> input1 = Arrays.asList(1,2,2,3,4,5,5,6);
+        List<Integer> input1 = Arrays.asList(1, 2, 2, 3, 4, 5, 5, 6);
 
         System.out.println(findOriginal(input1));
 
