fdc959523bec36f63245f1dac6ca9b85541d9714
Adilet <ninja2282011@gmail.com>
2025-03-25 19:52:38 +0600

    OriginalFinder

--- OriginalFinder.java
+++ OriginalFinder.java
@@ -21,7 +21,7 @@ public class OriginalFinder {
     }
 
     public static void main(String[] args) {
-        List<Integer> input1 = Arrays.asList(1, 2, 2, 3, 4, 5, 5, 6);
+        List<Integer> input1 = List.of(1, 2, 2, 3, 4, 5, 5, 6);
 
         System.out.println(findOriginal(input1));
 
